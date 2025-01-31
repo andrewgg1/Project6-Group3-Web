27e85a4719c0200b922f5a41dd93d55b419d781e
Andrew G <65138166+andrewgg1@users.noreply.github.com>
2025-01-31 13:31:56 -0500

    Refactor artist model to use embedded documents
    - Introduced embedded Song and Album classes
    - Updated Artist model to include embedded albums
    - Album model includes songs

--- models/artist.py
+++ models/artist.py
@@ -1,13 +1,23 @@
-from mongoengine import Document, StringField, IntField, ListField, ReferenceField
+from mongoengine import Document, StringField, IntField, ListField, EmbeddedDocument, EmbeddedDocumentListField
+
+class Song(EmbeddedDocument):
+    song_name = StringField(required=True)
+    song_length = IntField()
+    genre = StringField()
+
+class Album(EmbeddedDocument):
+    album_name = StringField(required=True)
+    release_year = IntField()
+    genre = StringField()
+    songs = EmbeddedDocumentListField(Song, default=list)
 
 class Artist(Document):
     artist_name = StringField(required=True)
     country_of_origin = StringField()
-    songs_credited = ListField(ReferenceField('Song'), default=list)
-    albums_credited = ListField(ReferenceField('Album'), default=list)
-    age = IntField(default=35)
+    age = IntField()
     genres = ListField(StringField(), default=list)
     label = StringField()
+    albums = EmbeddedDocumentListField(Album, default=list)
     meta = {
         'collection': 'artists'
     }
